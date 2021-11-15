6ab0f8ca0a5dfcd56017c87b1d53e8fbb35949a5
Ashe <63866529+Lethme@users.noreply.github.com>
2021-11-15 19:10:56 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ $(() => {
     let createEditFormTemplate = item => {
         return `
             <div class="modal fade" id="modalEditForm" tabindex="-1" aria-labelledby="modalEditFormLabel" aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-xl">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="modalEditFormLabel">` + item.title + `</h5>
